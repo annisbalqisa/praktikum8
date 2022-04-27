5bf7c1177e9c29021fa0b199fe1b3b5225864ddc
annisbalqisa <202410101045@mail.unej.ac.id>
2022-04-28 03:07:22 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-// $database = mysqli_connect('localhost', '202410101045', 'secret', 'uas202410101045');
-$database = mysqli_connect('localhost', 'root', '', 'sakila');
+$database = mysqli_connect('localhost', '202410101045', 'secret', 'uas202410101045');
+// $database = mysqli_connect('localhost', 'root', '', 'sakila');
